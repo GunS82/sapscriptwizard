a2e74be4cc98ea8abf1f797bafc9ac694d2c57c4
Gennady <guns1982@live.com>
2025-06-08 11:19:23 +0300

    Add run history tracking

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ from .shell_table import ShellTable
 # --- НОВЫЙ КОД ---
 from .gui_tree import GuiTree # Добавляем импорт нового класса
 # --- КОНЕЦ НОВОГО КОДА ---
+from .run_history import RunHistory
 from .types_.types import NavigateAction
 from .types_ import exceptions
 from .parallel import run_parallel, SapParallelRunner
